dcd2328d68e105da8ab641f756c68741a8749257
simondesrochers <simdesrochers@hotmail.com>
2020-11-26 11:28:47 -0500

    test

--- Hydra2.0/ContentView.swift
+++ Hydra2.0/ContentView.swift
@@ -11,16 +11,26 @@ struct ContentView: View {
     var body: some View {
         VStack{
             HStack{
-                    Image("hydraLogo").resizable().frame(width: 50, height: 50)
-                Text("Hydra").font(.system(size: 36)).foregroundColor(Color.white).padding(.leading,15)
-                Spacer()
-                Button(action:{
-                    print("Button pressed")
-                }){
-                    Image(systemName: "gear").resizable().frame(width: 50, height: 50).foregroundColor(.white).font(Font.system(.largeTitle).bold())
-                }
-            }.frame(minWidth:0, maxWidth: .infinity, minHeight: 70, alignment: .center).padding(10).background(LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .leading, endPoint: .trailing))
-        }.frame(minWidth:0,maxWidth: .infinity ,minHeight:0, maxHeight: .infinity, alignment: .topLeading)
+                    Image("hydraLogo").resizable().frame(width: 100, height: 100)
+                Text("Hydra").font(.custom("Optima-Bold", size: 36)).padding(.leading,15)
+                    Spacer()
+                    Button(action:{
+                        print("Button pressed")
+                    }){
+                    Image("settingIcon").resizable().frame(width: 50, height: 50)
+                    }
+            }.frame(minWidth:0, maxWidth: .infinity, minHeight: 100, alignment: .topLeading).padding(10).background(Color.gray)
+            
+            VStack(alignment:.leading){
+                Text("Serre").font(.custom("Optima-Bold" , size:36))
+                HStack{
+                    Text("30°C").font(.system(size: 24))
+                    Spacer()
+                    Text("60%").font(.system(size: 24))
+                    Text("Calvettes").font(.custom("Optima-Bold",size:36))
+                }.padding(15).background(Color.white).cornerRadius(10)
+            }.frame(alignment:.trailing).padding(20)
+        }.frame(minWidth:0,maxWidth: .infinity ,minHeight:0, maxHeight: .infinity, alignment: .topLeading).background(Color.blue)
         
     }
         
