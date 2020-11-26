0ef25c7cbfa496b6816b5ceaf9e64e06398a2d6f
simondesrochers <simdesrochers@hotmail.com>
2020-11-26 11:44:24 -0500

    changes

--- Hydra2.0/ContentView.swift
+++ Hydra2.0/ContentView.swift
@@ -20,17 +20,6 @@ struct ContentView: View {
                     Image(systemName: "gear").resizable().frame(width: 50, height: 50).foregroundColor(.white).font(Font.system(.largeTitle).bold())
                 }
             }.frame(minWidth:0, maxWidth: .infinity, minHeight: 70, alignment: .center).padding(10).background(LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .leading, endPoint: .trailing))
-        }.frame(minWidth:0,maxWidth: .infinity ,minHeight:0, maxHeight: .infinity, alignment: .topLeading)
-                    Image("hydraLogo").resizable().frame(width: 100, height: 100)
-                Text("Hydra").font(.custom("Optima-Bold", size: 36)).padding(.leading,15)
-                    Spacer()
-                    Button(action:{
-                        print("Button pressed")
-                    }){
-                    Image("settingIcon").resizable().frame(width: 50, height: 50)
-                    }
-            }.frame(minWidth:0, maxWidth: .infinity, minHeight: 100, alignment: .topLeading).padding(10).background(Color.gray)
-            
             VStack(alignment:.leading){
                 Text("Serre").font(.custom("Optima-Bold" , size:36))
                 HStack{
@@ -40,7 +29,12 @@ struct ContentView: View {
                     Text("Calvettes").font(.custom("Optima-Bold",size:36))
                 }.padding(15).background(Color.white).cornerRadius(10)
             }.frame(alignment:.trailing).padding(20)
-        }.frame(minWidth:0,maxWidth: .infinity ,minHeight:0, maxHeight: .infinity, alignment: .topLeading).background(Color.blue)
+        }.frame(minWidth:0,maxWidth: .infinity ,minHeight:0, maxHeight: .infinity, alignment: .topLeading)
+                    
+            
+            
+            
+        
         
     }
     
