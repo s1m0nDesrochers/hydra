3907c44e6e0998aca4a1acd7ab06bb2f163735e8
hubertmathieu <hmathieu53@gmail.com>
2020-12-02 09:33:56 -0500

    ui main page

--- Hydra2.0/ContentView.swift
+++ Hydra2.0/ContentView.swift
@@ -24,39 +24,52 @@ struct ContentView: View {
             VStack{
                 HStack{
                         Image("hydraLogo").resizable().frame(width: 50, height: 50)
-                    Text("Hydra").font(.largeTitle).foregroundColor(Color.white).padding(.leading,15)
+                    Text("Hydra").foregroundColor(Color.white).font(.custom("AppleSDGothicNeo-Light" ,size:36))
                     Spacer()
                     Button(action:{
                         print("allo")
                     }){
-                        Image(systemName: "gear").resizable().frame(width: 50, height: 50).foregroundColor(.white).font(Font.system(.largeTitle).bold())
+                        Image(systemName: "gear").resizable().frame(width: 30, height: 30).foregroundColor(.white).font(Font.system(.largeTitle).bold()).padding(.leading, 40)
                     }
                 }.frame(minWidth:0, maxWidth: .infinity, minHeight: 70, alignment: .center).padding(10).background(LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .leading, endPoint: .trailing))
-                VStack(alignment:.center , spacing:15 ){
-                    Image("serre").resizable().frame(width: 100, height: 100).cornerRadius(20).padding(15)
+                
+                VStack(alignment:.leading , spacing:15 ){
+                    
+                    VStack(alignment:.leading) {
+                        Text("Serre").font(.custom("AppleSDGothicNeo-Light" ,size:30))
+                    }
+                    
                     HStack{
-                        Text("\(String(format:"%.1f",viewModel.ambient.temp)) °C").font(.system(size: 24))
+                        Text("\(String(format:"%.1f",viewModel.ambient.temp)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
                         Spacer()
-                        Text("\(String(format:"%.1f",viewModel.ambient.humidity))%").font(.system(size: 24))
-                    }.frame(width:UIScreen.main.bounds.width - 40).padding(10).background(Color.white).opacity(0.6).cornerRadius(10)
-                    Text("Calvettes").font(.custom("Optima-Bold",size:36))
-                    HStack(spacing:10){
+                        Text("\(String(format:"%.1f",viewModel.ambient.humidity))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                    }.frame(width:UIScreen.main.bounds.width - 40).padding(10).background(Color.white).opacity(0.6).cornerRadius(5)
+                    
+                    VStack(alignment:.leading) {
+                        Text("Calvettes").font(.custom("AppleSDGothicNeo-Light" ,size:30))
+                    }
+                    
+                    HStack(){
                         VStack(spacing:10){
-                            Text("4 plantes")
-                            Text("\(String(format:"%.1f",viewModel.calvettes.temperature1)) °C")
-                            Text("\(String(format:"%.1f",viewModel.calvettes.humidity1))%")
-                        }.frame(width: 100,height:150,  alignment:.center).background(Color.white).opacity(0.6).cornerRadius(15)
+                            Text("4 plantes").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                            Text("\(String(format:"%.1f",viewModel.calvettes.temperature1)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                            Text("\(String(format:"%.1f",viewModel.calvettes.humidity1))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                        }.frame(width: 100,height:150,  alignment:.center).background(Color.white).opacity(0.6).cornerRadius(5)
+                        Spacer()
                         VStack(spacing:10){
-                            Text("2 plantes")
-                            Text("\(String(format:"%.1f",viewModel.calvettes.temperature1)) °C")
-                            Text("\(String(format:"%.1f",viewModel.calvettes.humidity2))%")
-                        }.frame(width: 100,height:150, alignment:.center).background(Color.white).opacity(0.6).cornerRadius(15)
+                            Text("2 plantes").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                            Text("\(String(format:"%.1f",viewModel.calvettes.temperature1)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                            Text("\(String(format:"%.1f",viewModel.calvettes.humidity2))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                        }.frame(width: 100,height:150, alignment:.center).background(Color.white).opacity(0.6).cornerRadius(5)
+                        Spacer()
                         VStack(spacing:10){
-                            Text("3 plantes")
-                            Text("\(String(format:"%.1f", viewModel.calvettes.temperature3)) °C")
-                            Text("\(String(format:"%.1f",viewModel.calvettes.humidity3))%")
-                        }.frame(width: 100,height:150, alignment: .center).background(Color.white).opacity(0.6).cornerRadius(15)
-                    }.padding(20)
+                            Text("3 plantes").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                            Text("\(String(format:"%.1f", viewModel.calvettes.temperature3)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                            Text("\(String(format:"%.1f",viewModel.calvettes.humidity3))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                        }.frame(width: 100,height:150, alignment: .center).background(Color.white).opacity(0.6).cornerRadius(5)
+                    
+                    }
+                    
                     HStack{
                         Image("waterTank").resizable().frame(width: 100, height: 100)
                         ZStack(alignment:.bottom){
@@ -64,8 +77,8 @@ struct ContentView: View {
                             Rectangle().frame(width: 10, height: CGFloat(viewModel.waterTank.waterLevel)).foregroundColor(Color.blue)
                         }
                         Text("Ph: \(String(format:"%.1f",viewModel.waterTank.ph))").font(.custom("Optima-Bold",size: 24)).padding(50)
-                        
                     }.frame(alignment:.bottom)
+                    
                     
                 }.frame(alignment:.center).padding(10)
             }.frame(minWidth:0,maxWidth: .infinity ,minHeight:UIScreen.main.bounds.height, maxHeight: .infinity, alignment: .topLeading).background(LinearGradient(gradient:Gradient(colors: [.white, .blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing))
