8a2ac15ac18a74c3927f948aabff5364dc56d420
hubertmathieu <hmathieu53@gmail.com>
2020-12-03 09:23:09 -0500

    uyvkjblk

--- Hydra2.0/Views/ContentView.swift
+++ Hydra2.0/Views/ContentView.swift
@@ -19,8 +19,6 @@ struct ContentView: View {
     
     var body: some View {
         
-        NavigationView{
-        
         ScrollView{
             
             VStack{
@@ -28,67 +26,94 @@ struct ContentView: View {
                         Image("hydraLogo").resizable().frame(width: 50, height: 50)
                     Text("Hydra").foregroundColor(Color.white).font(.custom("AppleSDGothicNeo-Light" ,size:36))
                     Spacer()
-                    NavigationLink(destination: InputView()){
-                        Image(systemName: "gear").resizable().frame(width: 30, height: 30).foregroundColor(.white).font(Font.system(.largeTitle).bold()).padding(.leading, 40)
-                            
+                    Button(action:{
+                        print("allo")
+                    }){
+                        Image(systemName: "gear").resizable().frame(width: 30, height: 30).foregroundColor(.white).font(Font.system(.largeTitle).bold())
                     }
-                    
-            }.frame(minWidth:0, maxWidth: .infinity, minHeight: 70, alignment: .center).padding(10).background(LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .leading, endPoint: .trailing))
+                }.frame(minWidth:0, maxWidth: .infinity, minHeight: 90, alignment: .center).padding(20).background(LinearGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color(hue: 0.5773694601403662, saturation: 0.9232538981610035, brightness: 0.7144907756024097, opacity: 0.8427025208990259), location: 0.0), Gradient.Stop(color: Color(hue: 0.3590735469955996, saturation: 0.7599689070000707, brightness: 0.7218876574412887, opacity: 0.8408396157873683), location: 1.0)]), startPoint: UnitPoint.leading, endPoint: UnitPoint.trailing))
                 
                 VStack(alignment:.leading , spacing:15 ){
                     
                     VStack(alignment:.leading) {
-                        Text("Serre").font(.custom("AppleSDGothicNeo-Light" ,size:30))
-                    }
+                        Text("Serre").font(.custom("AppleSDGothicNeo-Light" ,size:30)).foregroundColor(Color.white)
+                    }.padding(.top, 10)
                     
                     HStack{
-                        Text("\(String(format:"%.1f",viewModel.ambient.temp)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                        VStack(alignment: .leading) {
+                            Text("Température").font(.custom("AppleSDGothicNeo-Light", size: 12))
+                            Text("\(String(format:"%.1f",viewModel.ambient.temp)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                        }
                         Spacer()
-                        Text("\(String(format:"%.1f",viewModel.ambient.humidity))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
-                    }.frame(width:UIScreen.main.bounds.width - 40).padding(10).background(Color.white).opacity(0.6).cornerRadius(5)
+                        VStack(alignment: .trailing) {
+                            Text("Humidité").font(.custom("AppleSDGothicNeo-Light", size: 12))
+                            Text("\(String(format:"%.1f",viewModel.ambient.humidity))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                        }
+                    }.frame(width:UIScreen.main.bounds.width - 40).padding(10).background(Color.gray).cornerRadius(5)
                     
                     VStack(alignment:.leading) {
-                        Text("Calvettes").font(.custom("AppleSDGothicNeo-Light" ,size:30))
-                    }
+                        Text("Calvettes").font(.custom("AppleSDGothicNeo-Light" ,size:30)).foregroundColor(Color.white)
+                    }.padding(.top, 10)
                     
                     HStack(){
                         VStack(spacing:10){
                             Text("4 plantes").font(.custom("AppleSDGothicNeo-Light", size: 20))
                             Text("\(String(format:"%.1f",viewModel.calvettes.temperature1)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
                             Text("\(String(format:"%.1f",viewModel.calvettes.humidity1))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
-                        }.frame(width: 100,height:150,  alignment:.center).background(Color.white).opacity(0.6).cornerRadius(5)
+                        }.frame(width: 100,height:120,  alignment:.center).background(Color.gray).cornerRadius(5)
                         Spacer()
                         VStack(spacing:10){
                             Text("2 plantes").font(.custom("AppleSDGothicNeo-Light", size: 20))
                             Text("\(String(format:"%.1f",viewModel.calvettes.temperature1)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
                             Text("\(String(format:"%.1f",viewModel.calvettes.humidity2))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
-                        }.frame(width: 100,height:150, alignment:.center).background(Color.white).opacity(0.6).cornerRadius(5)
+                        }.frame(width: 100,height:120, alignment:.center).background(Color.gray).cornerRadius(5)
                         Spacer()
                         VStack(spacing:10){
                             Text("3 plantes").font(.custom("AppleSDGothicNeo-Light", size: 20))
                             Text("\(String(format:"%.1f", viewModel.calvettes.temperature3)) °C").font(.custom("AppleSDGothicNeo-Light", size: 20))
                             Text("\(String(format:"%.1f",viewModel.calvettes.humidity3))%").font(.custom("AppleSDGothicNeo-Light", size: 20))
-                        }.frame(width: 100,height:150, alignment: .center).background(Color.white).opacity(0.6).cornerRadius(5)
+                        }.frame(width: 100,height:120, alignment: .center).background(Color.gray).cornerRadius(5)
                     
                     }
                     
-                    HStack{
-                        Image("waterTank").resizable().frame(width: 100, height: 100)
-                        ZStack(alignment:.bottom){
-                            Rectangle().frame(width: 10, height: 100).foregroundColor(Color.gray)
-                            Rectangle().frame(width: 10, height: CGFloat(viewModel.waterTank.progress)).foregroundColor(Color.blue)
+                    VStack(alignment:.leading) {
+                        HStack() {
+                            Text("Réservoire").font(.custom("AppleSDGothicNeo-Light" ,size:30)).foregroundColor(Color.white)
+                            Image("waterTank").resizable().frame(width: 25, height: 25)
+                            ZStack(alignment:.bottom){
+                                Rectangle().frame(width: 10, height: 25).foregroundColor(Color.gray)
+                                Rectangle().frame(width: 10, height: CGFloat(viewModel.waterTank.progress)).foregroundColor(Color.blue)
+                            }
                         }
+                    }.padding(.top, 10)
+                    HStack() {
+                        VStack(alignment: .leading) {
+                            Text("PH").font(.custom("AppleSDGothicNeo-Light", size: 12))
+                            Text("7.5").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                            Spacer()
+                            Text("Flora Gro").font(.custom("AppleSDGothicNeo-Light", size: 12))
+                            Text("12 ml/l").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                        }
+                        Spacer()
+                        VStack(alignment: .trailing) {
+                            Text("Flora Bloom").font(.custom("AppleSDGothicNeo-Light", size: 12))
+                            Text("20 ml/l").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                            Spacer()
+                            Text("Flora Micro").font(.custom("AppleSDGothicNeo-Light", size: 12))
+                            Text("5 ml/l").font(.custom("AppleSDGothicNeo-Light", size: 20))
+                        }
+                    }.frame(width:UIScreen.main.bounds.width - 40).padding(10).background(Color.gray).cornerRadius(5)
+                    
+                    
+                    HStack{
+                        
                         Text("Ph: \(String(format:"%.1f",viewModel.waterTank.ph))").font(.custom("Optima-Bold",size: 24)).padding(50)
                     }.frame(alignment:.bottom)
                     
                     
-                }.frame(alignment:.center).padding(10)
-            }.frame(minWidth:0,maxWidth: .infinity ,minHeight:UIScreen.main.bounds.height, maxHeight: .infinity, alignment: .topLeading).background(LinearGradient(gradient:Gradient(colors: [.white, .blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing))
-            
+                }.frame(alignment:.center).padding(15)
+            }.background(RadialGradient(gradient: Gradient(stops: [Gradient.Stop(color: Color(hue: 0.5773694601403662, saturation: 0.9232538981610035, brightness: 0.7144907756024097, opacity: 0.8427025208990259), location: 0.0), Gradient.Stop(color: Color(hue: 0.3590735469955996, saturation: 0.7599689070000707, brightness: 0.0, opacity: 1.0), location: 1.0)]), center: UnitPoint.topLeading, startRadius: 0.0, endRadius: 174.96719360351562))
         }
-        
-        }
-        
     }
     
 }
